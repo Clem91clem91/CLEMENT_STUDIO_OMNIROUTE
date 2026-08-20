0c54994c4e6983dec7c71487a2c37b30c2fb8560
Clem91clem91 <138784225+Clem91clem91@users.noreply.github.com>
2026-08-20 15:24:02 +0200

    feat: expose OmniRoute certification primitives

---
+++ src/clement_omniroute/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    AccountingDecision,
+    EndpointKind,
+    EndpointSpec,
+    classify_endpoint,
+    decide_accounting,
+)
+
+__all__ = [
+    "AccountingDecision",
+    "EndpointKind",
+    "EndpointSpec",
+    "classify_endpoint",
+    "decide_accounting",
+]
+
+__version__ = "0.1.0"
